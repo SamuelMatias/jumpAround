37e3ce9215800adad441b7f886df0b383192d328
Bruno Rebelo <rebelob@hotmail.com>
2019-08-13 21:11:43 +0000

    Finished Feature IataConverter

---
+++ src/main/java/org/academiadecodigo/codezillas/converters/IataToLocationConverter.java
@@ -0,0 +1,18 @@
+package org.academiadecodigo.codezillas.converters;
+
+public class IataToLocationConverter {
+    Airports[] airports;
+
+    public IataToLocationConverter(){
+        airports = Airports.values();
+    }
+
+    public String converter(String Iata) {
+        for (int i = 0; i < airports.length ; i++) {
+            if(airports[i].name() == Iata){
+                return airports[i].getLocationServed();
+            }
+        }
+        return "NoAirport";
+    }
+}
