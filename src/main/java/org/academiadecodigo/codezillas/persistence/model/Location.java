8bd7c065f03ecef193b3c4e1d2efb64f5c4aa513
Pedro Ventura <pedromariacostapinto@gmail.com>
2019-08-13 15:59:52 +0000

    repository DP, Users(class) and Location(class)

---
+++ src/main/java/org/academiadecodigo/codezillas/persistence/model/Location.java
@@ -0,0 +1,23 @@
+package org.academiadecodigo.codezillas.persistence.model;
+
+public class Location extends AbstractModel{
+
+    private String country;
+    private String city;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
