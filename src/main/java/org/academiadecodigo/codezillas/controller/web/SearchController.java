02884aa8237d458b9f42d3d8bd5e8e894d9f9d09
Bruno Rebelo <rebelob@hotmail.com>
2019-08-14 03:19:22 +0000

    ResultView

--- src/main/java/org/academiadecodigo/codezillas/controller/web/SearchController.java
+++ src/main/java/org/academiadecodigo/codezillas/controller/web/SearchController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -34,16 +35,16 @@ public class SearchController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "")
-    public String findDestination(@Valid @ModelAttribute("searchDto") SearchDto searchDto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String findDestination(@Valid @ModelAttribute("searchDto") SearchDto searchDto, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         if (bindingResult.hasErrors()) {
             redirectAttributes.addFlashAttribute("lastAction", "Make a new search");
             return "redirect:/search/";
         }
 
-        //return searchService.searchDestinations(searchDto);
-       // return searchService.makeRequest(searchDto);
-        return "index";
+        model.addAttribute("destinations", searchService.searchDestinations(searchDto));
+       //return searchService.makeRequest(searchDto);
+        return "result";
     }
 
 }
